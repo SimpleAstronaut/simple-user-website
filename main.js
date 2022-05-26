4dddf9717ea6dbd6c52cfd32578de6daff0a1a17
SimpleAstronaut <escscience@163.com>
2022-05-26 09:23:48 +0800

    修改接口地址

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
 function login() {
     const username = document.getElementById("username").value;
-    $.get("http://127.0.0.1:8082/getid", { username: username }, function (data, status) {
+    $.get("https://service-jzdhwuy1-1304083067.bj.apigw.tencentcs.com/release/getid", { username: username }, function (data, status) {
         data = JSON.parse(data);
         const sta = data.status;
         if (sta == 200) {
             const user_id = data.USER_ID;
             const password = document.getElementById("password").value;
-            $.get("http://127.0.0.1:8081/token/get", { id: user_id, password: password }, function (data, status) {
+            $.get("https://service-2v0ldrgr-1304083067.bj.apigw.tencentcs.com/release/token/get", { id: user_id, password: password }, function (data, status) {
                 var tokensta = data.status;
                 if (tokensta == 200) {
                     const token = data.token;
                     if (getQueryString("target") == null) {
-                        const target = "http://127.0.0.1:5500/admin.html?user_id=" + user_id + "&token=" + token;
+                        const target = "https://my-bucket-1irmn14-1304083067.cos-website.ap-beijing.myqcloud.com/admin.html?user_id=" + user_id + "&token=" + token;
                         window.location.href = target;
                     } else {
                         const target = getQueryString("target") + "?user_id=" + user_id + "&token=" + token;
